6febc7456a04e099b5097589eb8d5dcefbb7c702
hao nguyen <hao.nguyenhuu@ggg.com.vn>
2024-10-04 18:07:28 +0700

    create docker for k8s

--- kubernetes/kubeadm/bootstrap_master_1.sh
+++ kubernetes/kubeadm/bootstrap_master_1.sh
@@ -4,10 +4,10 @@ echo "[TASK 1] Pull required containers"
 kubeadm config images pull
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-kubeadm init --control-plane-endpoint="192.168.201.49:6443" --upload-certs --apiserver-advertise-address=192.168.201.154 --pod-network-cidr=192.168.0.0/16
+kubeadm init --control-plane-endpoint="192.168.201.49:6443" --upload-certs --apiserver-advertise-address=192.168.201.154 --pod-network-cidr=10.87.0.0/16
 echo "[TASK 3] Deploy Calico network"
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f e
 
 # You can now join any number of the control-plane node running the following command on each as root:
 # IMPORTANT: You also need to pass --apiserver-advertise-address to the join command when you join the other master node.
